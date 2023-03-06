7de82187a16dcbb58e9cbaa19bb0bc3c84dddce9
shazzy-kawa <shaz_saleh@live.co.uk>
2023-03-06 12:32:02 +0000

    added to index.js file

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./Manager");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,8 +8,134 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+const render = require("./page-template");
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+function createEngineer(team) {
+    inquirer.prompt([
+        
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the engineer's id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's email?",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's GitHub username?",
+        }
+    ]).then((engineerDetails) => {
+       
+        const engineer = new Engineer(engineerDetails.name, engineerDetails.id, engineerDetails.email, engineerDetails.github)
+        team.push(engineer);
+        createTeam(team); 
+    });
+}
 
+function createIntern(team) {
+    inquirer.prompt([
+        // Intern 
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the Intern's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the Intern's id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the Intern's email?",
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the Intern's school?",
+        }
+    ]).then((internDetails) => {
+        const intern = new Intern(internDetails.name, internDetails.id, internDetails.email, internDetails.school)
+         team.push(intern);
+        createTeam(team); 
+    });
+}
+
+function createTeam(team) {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'memberChoice',
+            message: 'Which type of team member you want to add?',
+            choices: [
+                'Engineer',
+                'Intern',
+                "I don't want to add any more team members",
+            ],
+        }
+    ]).then((choice) => {
+        if (choice.memberChoice === 'Engineer') {
+            createEngineer(team);
+        } else if (choice.memberChoice === 'Intern') {
+            createIntern(team);
+        } else {
+            
+            const html = render(team); 
+            
+            fs.writeFile(outputPath, html, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+    });
+}
+
+function createManager(team) {
+    inquirer.prompt([
+        
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the team manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's id?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email?",
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the manager's office phone number?",
+        }
+    ]).then((managerDetails) => {
+      
+        const manager = new Manager(managerDetails.name, managerDetails.id, managerDetails.email, managerDetails.officeNumber)
+        team.push(manager);
+        createTeam(team); 
+    });
+}
+
+function start() {
+    const team = []; 
+    createManager(team);
+}
+
+start();
